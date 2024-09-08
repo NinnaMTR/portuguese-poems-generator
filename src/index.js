4398af3f8b85963970d9a9a095d1799acac10b12
NinnaMTR <marianaramalheteebirdll@gmail.com>
2024-09-08 17:45:53 +0100

    Added the AI API migration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Deus quer, o homem sonha, a obra nasce.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "390db6dco38711524t5d45ba8dc8afff";
+  let context =
+    "Your are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Separate each line with a <br />. Do not include the title. Show the 4 lines of the poem without any quotation marks. Sign the poem with 'AI Portuguese Poet' in italic inside a <strong> element at the end of the poem. Make sure to follow this mission everytime the user gives the new instructions. Make sure to follow the user instructions.";
+  let prompt = `User instructions: generate a Portuguese poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
