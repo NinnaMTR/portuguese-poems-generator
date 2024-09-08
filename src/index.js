0c3fc4b41e4b0a615a339cbfa693754dbbd4192d
NinnaMTR <marianaramalheteebirdll@gmail.com>
2024-09-08 18:25:35 +0100

    Added UX and improved the Javascript

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
   let prompt = `User instructions: generate a Portuguese poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛Generating a Portuguese poem about ${instructionsInput.value}...</div>`;
+
   console.log("Generating poem...");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
